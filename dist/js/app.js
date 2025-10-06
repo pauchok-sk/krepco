746f5642365da7d165fbbbf3db39d9bfc385c4d9
pauchok-sk <pauchoooooook@gmail.com>
2025-10-06 16:31:56 +0300

    switcher section dev

--- dist/js/app.js
+++ dist/js/app.js
@@ -345,6 +345,35 @@
                 }
             });
         }
+        const switcherSliders = document.querySelectorAll(".section-switcher__slider");
+        if (switcherSliders.length) switcherSliders.forEach(slider => {
+            new Swiper(slider, {
+                speed: 700,
+                slidesPerView: 5,
+                spaceBetween: 20,
+                navigation: {
+                    prevEl: slider.closest(".section-switcher__tab-wrapper").querySelector(".section-switcher__slider-btn._prev"),
+                    nextEl: slider.closest(".section-switcher__tab-wrapper").querySelector(".section-switcher__slider-btn._next")
+                },
+                pagination: {
+                    el: ".section-hero__slider-pagination",
+                    clickable: true
+                }
+            });
+        });
+        const productSliders = document.querySelectorAll(".card-product__slider");
+        if (productSliders.length) productSliders.forEach(slider => {
+            new Swiper(slider, {
+                speed: 700,
+                autoplay: {
+                    delay: 3500
+                },
+                pagination: {
+                    el: slider.nextElementSibling,
+                    clickable: true
+                }
+            });
+        });
     }
     function spoller() {
         const spollersArray = document.querySelectorAll("[data-spollers]");
--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -6,12 +6,58 @@ export default function sliders() {
       speed: 700,
       effect: "fade",
       autoplay: {
-        delay: 3500
+        delay: 3500,
       },
       pagination: {
         el: ".section-hero__slider-pagination",
-        clickable: true
-      }
-    })
+        clickable: true,
+      },
+    });
+  }
+
+  const switcherSliders = document.querySelectorAll(
+    ".section-switcher__slider"
+  );
+
+  if (switcherSliders.length) {
+    switcherSliders.forEach((slider) => {
+      const swiper = new Swiper(slider, {
+        speed: 700,
+        slidesPerView: 5,
+        spaceBetween: 20,
+        // autoplay: {
+        //   delay: 3200,
+        // },
+        navigation: {
+          prevEl: slider
+            .closest(".section-switcher__tab-wrapper")
+            .querySelector(".section-switcher__slider-btn._prev"),
+          nextEl: slider
+            .closest(".section-switcher__tab-wrapper")
+            .querySelector(".section-switcher__slider-btn._next"),
+        },
+        pagination: {
+          el: ".section-hero__slider-pagination",
+          clickable: true,
+        },
+      });
+    });
+  }
+
+  const productSliders = document.querySelectorAll(".card-product__slider");
+
+  if (productSliders.length) {
+    productSliders.forEach((slider) => {
+      const swiper = new Swiper(slider, {
+        speed: 700,
+        autoplay: {
+          delay: 3500,
+        },
+        pagination: {
+          el: slider.nextElementSibling,
+          clickable: true,
+        },
+      });
+    });
   }
 }
