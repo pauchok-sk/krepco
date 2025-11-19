c803c5e2f7e4f22fa2473cb6c9fd8c22fef17971
pauchok-sk <pauchoooooook@gmail.com>
2025-11-19 22:56:23 +0300

    repair sizes fonts

--- dist/js/app.js
+++ dist/js/app.js
@@ -268,20 +268,34 @@
         const im_seven = new Inputmask("+7 (999) 999-99-99");
         const im_eight = new Inputmask("9 (999) 999-99-99");
         inputs.forEach(input => {
+            let isFirstKey = true;
             input.addEventListener("focus", e => {
                 const value = e.target.value;
-                if (!value) input?.inputmask?.remove();
+                if (!value) {
+                    input?.inputmask?.remove();
+                    isFirstKey = true;
+                }
             });
             input.addEventListener("blur", e => {
                 const value = e.target.value;
-                if (!value) input.placeholder = "+7 (999) 999-99-99";
+                if (!value) {
+                    input.placeholder = "+7 (999) 999-99-99";
+                    isFirstKey = true;
+                }
             });
             input.addEventListener("keydown", e => {
                 const value = e.target.value.replace(/\D/g, "");
-                if (e.key === "8") im_eight.mask(input); else if (e.key !== "Backspace") im_seven.mask(input);
-                if (e.key === "Backspace" && (value === "7" || value === "8")) {
+                if (isFirstKey) {
+                    if (e.key === "8") im_eight.mask(input); else if (e.key !== "Backspace") {
+                        if (e.key === "7") e.preventDefault();
+                        im_seven.mask(input);
+                    }
+                    isFirstKey = false;
+                }
+                if (e.key === "Backspace" && (value === "7" || value === "8" || !value)) {
                     input?.inputmask?.remove();
                     input.value = "";
+                    isFirstKey = true;
                 }
             });
         });
